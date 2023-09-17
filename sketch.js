d58290488a0a44b92639cb9a93fba8a3db67d980
Shashwat Mishra <144282119+smx64@users.noreply.github.com>
2023-09-17 00:31:46 -0400

    Pattern Recreation - V1

--- sketch.js
+++ sketch.js
@@ -1,5 +1,31 @@
 function setup() {
+    createCanvas(750,750);
+    background('#303030');
 }
 
 function draw() {
+    strokeWeight(0);   
+    fill('#F1E9D2')
+    beginShape();       //shape-left
+        vertex(170,0);              //p1: top left
+        vertex(0,400);              //p2: bottom left
+        vertex(200,400);            //p3: bottom middle
+        vertex(200,200);            //p4: middle left
+        vertex(300,200);            //p5: middle right
+        vertex(390,400);            //p6: bottom right
+        vertex(540,0);              //p7: top right
+    endShape(CLOSE);
+    
+    translate(width+30,height+55);
+    rotate(PI);
+    
+    beginShape();      //shape-right
+        vertex(170,0);              //p1: top left
+        vertex(0,400);              //p2: bottom left
+        vertex(200,400);            //p3: bottom middle
+        vertex(200,200);            //p4: middle left
+        vertex(300,200);            //p5: middle right
+        vertex(390,400);            //p6: bottom right
+        vertex(540,0);              //p7: top right
+    endShape(CLOSE);
 }
