c0af8c690a242607d6a82713cab4f981eddc7924
Shashwat Mishra <144282119+smx64@users.noreply.github.com>
2023-09-21 01:03:46 -0400

    Updated Code For Better Symmetry

--- sketch.js
+++ sketch.js
@@ -8,24 +8,24 @@ function draw() {
     fill('#F1E9D2')
     beginShape();       //shape-left
         vertex(170,0);              //p1: top left
-        vertex(0,400);              //p2: bottom left
-        vertex(200,400);            //p3: bottom middle
+        vertex(0,375);              //p2: bottom left
+        vertex(200,375);            //p3: bottom middle
         vertex(200,200);            //p4: middle left
         vertex(300,200);            //p5: middle right
-        vertex(390,400);            //p6: bottom right
+        vertex(375,375);            //p6: bottom right
         vertex(540,0);              //p7: top right
     endShape(CLOSE);
     
-    translate(width+30,height+55);
+    translate(width,height);
     rotate(PI);
     
     beginShape();      //shape-right
         vertex(170,0);              //p1: top left
-        vertex(0,400);              //p2: bottom left
-        vertex(200,400);            //p3: bottom middle
+        vertex(0,375);              //p2: bottom left
+        vertex(200,375);            //p3: bottom middle
         vertex(200,200);            //p4: middle left
         vertex(300,200);            //p5: middle right
-        vertex(390,400);            //p6: bottom right
+        vertex(375,375);            //p6: bottom right
         vertex(540,0);              //p7: top right
     endShape(CLOSE);
 }
